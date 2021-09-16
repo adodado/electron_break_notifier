6a74867db94432cfcd58a562a37fc136d0fc6d99
Admir Cosic <admir.cosic.82@gmail.com>
2021-09-16 14:12:06 +0200

    Finished example logic.

--- index.js
+++ index.js
@@ -1,45 +1,68 @@
 const { app, Tray, Menu } = require("electron");
-const path = require("path");
-const iconPath = path.join(__dirname, "icon.png");
-var opsys = process.platform;
-let trayApp = null;
-var status = "enabled"; //Application is in disabled mode at start up
-var intervalID;
-var interval = 0;
 const WindowsBalloon = require("node-notifier").WindowsBalloon;
 const notifier = require("node-notifier");
+const path = require("path");
+const iconPath = path.join(__dirname, "icon.png");
+let trayApp = null;
+let opsys = process.platform;
+let status = "enabled";
+let setInterval;
+let notificationMilisecondInterval = 0;
+const appname = "BreakTime Buddy";
 
-var winNotifier = new WindowsBalloon({
+const notifications = [
+  "It’s okay to take a break. So why not do exactly that right now.",
+  "Disconnect to reconnect. You need to take a break ASAP.",
+  "Production environment is crashing and everything is going up in flames. So this is the perfect time for you to take a break.",
+  "Relax. Nothing is under control. Step away from the keyboard. Rest your mind. Calm your heart.",
+  "If you’re tired, learn to rest not quit. And what better time to do just that than now.",
+  "For the love of your work, please take a break ASAP! Its break time!",
+  "Raise your hand if you need a break from your work! Even if you did not raise your hand it is time for a small break.",
+  "Sometimes giving yourself a break is the very thing you need. And i think you need that right now.",
+]
+
+const winNotifier = new WindowsBalloon({
   withFallback: false,
   customPath: void 0,
 });
 
-function sendNotification() {
-  console.log(winNotifier);
-  if (opsys == "darwin") {
-    opsys = "MacOS";
-  } else if (opsys == "win32" || opsys == "win64") {
+const toMiliseconds = (minutes) => {
+  return 60000 * minutes;
+};
+
+const intervalSelected = (minutes) => {
+  if (notificationMilisecondInterval === toMiliseconds(minutes) || status === "disabled") {
+    notificationMilisecondInterval = 0;
+    clearInterval(setInterval);
+  } else {
+    notificationMilisecondInterval = toMiliseconds(minutes);
+    startNewInterval();
+  }
+};
+
+const sendNotification = () => {
+  if (opsys == "win32" || opsys == "win64") {
     winNotifier.notify(
       {
-        title: "BreakTime Buddy",
-        message: "Test 1",
+        title: appname,
+        message: notifications[~~(Math.random() * notifications.length)],
         sound: true,
-        wait: true, // Wait for User Action against Notification
-        type: "warn", // The notification type : info | warn | error
+        wait: false, // Wait for User Action against Notification
+        type: "info", // The notification type : info | warn | error
       },
       function (err, response) {
         console.log(err);
         console.log(response);
       }
     );
-  } else if (opsys == "linux") {
+  } else {
     notifier.notify(
       {
-        title: "BreakTime Buddy",
-        message: "Test 1",
+        title: appname,
+        message: notifications[~~(Math.random() * notifications.length)],
         sound: true,
-        wait: true, // Wait for User Action against Notification
-        type: "warn", // The notification type : info | warn | error
+        wait: false, // Wait for User Action against Notification
+        type: "info", // The notification type : info | warn | error
       },
       function (err, response) {
         console.log(err);
@@ -47,44 +70,28 @@ function sendNotification() {
       }
     );
   }
-}
-function startNewInterval() {
-  clearInterval(intervalID);
-  if (status === "enabled") {
-    intervalID = setInterval(function () {
-      sendNotification();
-    }, interval);
-  }
-}
+};
 
-function getContextMenu() {
+const startNewInterval = () => {
+  clearInterval(setInterval);
+  setInterval = setInterval(function () {
+    sendNotification();
+  }, notificationMilisecondInterval);
+};
+
+const getContextMenu = () => {
   let contextMenu = Menu.buildFromTemplate([
     {
-        label: "Enable",
-        type: "checkbox",
-        checked: status == "enabled",
-        click: function () {
-          if (status === "disabled") {
-            status = "enabled";
-          } else {
-            status = "disabled";
-            clearInterval(intervalID);
-            interval=0;
-          }
-          trayApp.setContextMenu(getContextMenu());
-        },
-      },
-    {
-      label: "10 minutes",
+      label: "Enable",
       type: "checkbox",
-      checked: interval == (60000 * 10),
+      checked: status == "enabled",
       click: function () {
-        if (interval === (60000 * 10)) {
-          interval = 0;
-          clearInterval(intervalID);
+        if (status === "disabled") {
+          status = "enabled";
         } else {
-            interval=(60000 * 10);
-            startNewInterval();
+          status = "disabled";
+          clearInterval(setInterval);
+          notificationMilisecondInterval = 0;
         }
         trayApp.setContextMenu(getContextMenu());
       },
@@ -96,6 +103,51 @@ function getContextMenu() {
       },
     },
     {
+      label: "10 minutes",
+      type: "checkbox",
+      checked: notificationMilisecondInterval == toMiliseconds(10),
+      click: function () {
+        intervalSelected(10);
+        trayApp.setContextMenu(getContextMenu());
+      },
+    },
+    {
+      label: "30 minutes",
+      type: "checkbox",
+      checked: notificationMilisecondInterval == toMiliseconds(30),
+      click: function () {
+        intervalSelected(30);
+        trayApp.setContextMenu(getContextMenu());
+      },
+    },
+    {
+      label: "1 hour",
+      type: "checkbox",
+      checked: notificationMilisecondInterval == toMiliseconds(60),
+      click: function () {
+        intervalSelected(60);
+        trayApp.setContextMenu(getContextMenu());
+      },
+    },
+    {
+      label: "1.5 hour",
+      type: "checkbox",
+      checked: notificationMilisecondInterval == toMiliseconds(90),
+      click: function () {
+        intervalSelected(90);
+        trayApp.setContextMenu(getContextMenu());
+      },
+    },
+    {
+      label: "2 hours",
+      type: "checkbox",
+      checked: notificationMilisecondInterval == toMiliseconds(120),
+      click: function () {
+        intervalSelected(120);
+        trayApp.setContextMenu(getContextMenu());
+      },
+    },
+    {
       label: "Exit",
       click: function () {
         app.exit();
@@ -103,11 +155,11 @@ function getContextMenu() {
     },
   ]);
   return contextMenu;
-}
+};
 
 app.on("ready", () => {
   trayApp = new Tray(iconPath);
   var contextMenu = getContextMenu();
-  trayApp.setToolTip("BreakTime Buddy");
+  trayApp.setToolTip(appname);
   trayApp.setContextMenu(contextMenu);
 });
